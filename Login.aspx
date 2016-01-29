4aa6ccadbb060000140b8235f57216c63cd75e17
VoineaBogdan <spawnvoinea@gmail.com>
2016-01-29 04:59:59 +0200

    Web Form Login

---
+++ Login.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="Log in" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="WebApplication3.Account.Login" %>
+
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <table class="auto-style1">
+        <tr>
+            <td class="auto-style4">
+                <asp:TextBox ID="TextBoxUser" runat="server" Width="200px"></asp:TextBox>
+            </td>
+            <td class="auto-style3">
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Insert a user name" Font-Bold="True" Font-Strikeout="False" ForeColor="Red" ControlToValidate="TextBoxUser"></asp:RequiredFieldValidator>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style4">
+                <asp:TextBox ID="TextBoxPassword" runat="server" Width="200px"></asp:TextBox>
+            </td>
+            <td class="auto-style3">
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="Insert a password" Font-Bold="True" Font-Strikeout="False" ForeColor="Red" ControlToValidate="TextBoxPassword"></asp:RequiredFieldValidator>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style4">
+                <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Login" />
+            </td>
+            <td class="auto-style3">
+                &nbsp;</td>
+        </tr>
+        </table>
+</asp:Content>
+<asp:Content ID="Content1" runat="server" contentplaceholderid="HeadContent">
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+            margin-bottom: 0px;
+        }
+        .auto-style3 {
+            width: 143px;
+        }
+        .auto-style4 {
+            width: 5px;
+        }
+    </style>
+</asp:Content>
+
---
+++ Login.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplication3.Account
+{
+    public partial class Login : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connect = new SqlConnection("Data Source=(local);Initial Catalog=ProiectBaze;Integrated Security=SSPI;Packet Size=3500;Connection Timeout=60 ");
+            connect.Open();
+            string checkuser = "select count(*) from LogInn where username='" + TextBoxUser.Text + "'";
+            SqlCommand com = new SqlCommand(checkuser, connect);
+
+            int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+            connect.Close();
+
+            if (temp == 1)
+            {
+
+                connect.Open();
+                string checkpass = "select parola from LogInn where username='" +TextBoxUser.Text + "'";
+                SqlCommand passCom = new SqlCommand(checkpass, connect);
+                string password = passCom.ExecuteScalar().ToString().Replace(" ", "");
+                if (temp == 1)
+                {
+                    Response.Write(password);
+                }
+                if (password == TextBoxPassword.Text)
+                {
+                    Session["New"] = TextBoxUser.Text;
+                    Response.Write("Password is corect");
+                }
+                else
+                {
+                    Response.Write("Password is not corect");
+                }
+
+            }
+            else
+            {
+                Response.Write("Username is NOT corect");
+            }
+
+        }
+
+ 
+      
+    }
+}
